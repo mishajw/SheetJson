8555f9d1f81f543105bb5a1217da9cd92849095f
Misha Wagner <mishajw@gmail.com>
2016-06-13 20:47:17 +0100

    Add JavaDoc

--- src/main/scala/music2/util/Output.scala
+++ src/main/scala/music2/util/Output.scala
@@ -7,15 +7,27 @@ object Output {
 
   val sampleRate = 24000
 
+  /**
+    * Input audio is read in these size chunks
+    */
   private val bufferAmount = 64
 
+  /**
+    * Bytes are read from this queue
+    */
   private val byteQueue =
     new LinkedBlockingQueue[Byte]()
 
+  /**
+    * The thread that plays bytes
+    */
   private val thread = new Thread(new Runnable {
     def run() = playFromQueue()
   })
 
+  /**
+    * @return the default audio line
+    */
   private def defaultLine = {
     val format =
       // TODO: Check the parameters
@@ -33,12 +45,19 @@ object Output {
     line
   }
 
+  /**
+    * Destroy a line
+    * @param line
+    */
   private def destroy(line: SourceDataLine) = {
     line.drain()
     line.stop()
     line.close()
   }
 
+  /**
+    * Read from the byte queue, and play the results
+    */
   private def playFromQueue() = {
     implicit val line = defaultLine
     var running = true
@@ -61,13 +80,27 @@ object Output {
     destroy(line)
   }
 
+  /**
+    * Play bytes to audio
+    * @param bytes the bytes to play
+    * @param line the line to play them from
+    */
   private def playBytes(bytes: Traversable[Byte])(implicit line: SourceDataLine) =
     line.write(bytes.toArray, 0, bytes.size - 1)
 
+  /**
+    * Start the playing
+    */
   def start(): Unit = thread.start()
 
+  /**
+    * Play an integer
+    */
   def play(x: Int) = byteQueue add x.toByte
 
+  /**
+    * Stop the playing
+    */
   def stop() = thread.synchronized {
     thread.interrupt()
     thread.join()
