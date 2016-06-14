2801219bfc80a04878c634e64d7e179a028d87d7
Misha Wagner <mishajw@gmail.com>
2016-06-14 15:48:44 +0100

    Implement file writing for output

--- src/main/scala/music2/output/FileOut.scala
+++ src/main/scala/music2/output/FileOut.scala
@@ -1,6 +1,17 @@
 package music2.output
+import java.io.{BufferedOutputStream, FileOutputStream}
+
 import music2.player.PlayableImplicits.Playable
 
 class FileOut(val path: String) extends Out {
-  override def play[T](x: T)(implicit p: Playable[T]): Unit = ???
+
+  val pw = new BufferedOutputStream(new FileOutputStream(path))
+
+  override def play[T](x: T)(implicit p: Playable[T]): Unit = {
+    pw.write(p.toBytes(x).toArray)
+  }
+
+  def stop() = {
+    pw.close()
+  }
 }
--- src/main/scala/music2/output/SoundAndFileOut.scala
+++ src/main/scala/music2/output/SoundAndFileOut.scala
@@ -5,8 +5,17 @@ class SoundAndFileOut(val path: String) extends Out {
 
   val fileOut = new FileOut(path)
 
+  def start() = {
+    SoundOut.start()
+  }
+
   override def play[T](x: T)(implicit p: Playable[T]): Unit = {
     fileOut.play(x)
     SoundOut.play(x)
+  }
+
+  def stop() = {
+    SoundOut.stop()
+    fileOut.stop()
   }
 }
