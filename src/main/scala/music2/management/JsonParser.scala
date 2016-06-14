24b9003196e1677e8ea10cf9e6bcc3a26b64e7ec
Misha Wagner <mishajw@gmail.com>
2016-06-14 16:29:49 +0100

    Add JavaDoc

--- src/main/scala/music2/management/JsonParser.scala
+++ src/main/scala/music2/management/JsonParser.scala
@@ -5,18 +5,31 @@ import music2.player.Player
 import scala.io.Source
 
 object JsonParser {
+
+  /**
+    * Parse JSON from a file
+    * @param path path of the file
+    * @tparam T the type of Player to return
+    * @return an option of a player from the JSON file
+    */
   def parse[T](path: String): Option[Player[T]] = {
     try {
       val f = Source.fromFile(path)
       val raw = f.mkString
       f.close()
 
-      Some(parseRaw(raw))
+      parseRaw(raw)
     } catch {
       // TODO: Catch only relevant exceptions
       case e: Exception => None
     }
   }
 
-  def parseRaw[T](json: String): Player[T] = ???
+  /**
+    * Parse a JSON string
+    * @param json the JSON string
+    * @tparam T the type of Player to return
+    * @return an option of a player from the JSON string
+    */
+  def parseRaw[T](json: String): Option[Player[T]] = ???
 }
