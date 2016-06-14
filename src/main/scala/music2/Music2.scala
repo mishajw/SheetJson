40136c446452028b00354b3d1de879647bb15230
Misha Wagner <mishajw@gmail.com>
2016-06-14 14:31:00 +0100

    Change main method to use SoundOut

--- src/main/scala/music2/Music2.scala
+++ src/main/scala/music2/Music2.scala
@@ -1,19 +1,19 @@
 package music2
 
+import music2.output.SoundOut
 import music2.player.SimpleTone
-import music2.util.Output
 
 object Music2 {
   def main(args: Array[String]) {
     val st = new SimpleTone(261.63)
 
-    val played = for (_ <- 0 until Output.sampleRate * 3)
+    val played = for (_ <- 0 until SoundOut.sampleRate * 3)
       yield st.play
 
-    played.foreach(Output.play(_))
+    played.foreach(SoundOut.play(_))
 
-    Output start()
+    SoundOut start()
     Thread.sleep(5000)
-    Output stop()
+    SoundOut stop()
   }
 }
