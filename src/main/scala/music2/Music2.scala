e1ad32e34b0b15db73eeeef482f53581d3e37d17
Misha Wagner <mishajw@gmail.com>
2016-06-14 20:45:06 +0100

    Add argument parsing in main method for loading JSON files

--- src/main/scala/music2/Music2.scala
+++ src/main/scala/music2/Music2.scala
@@ -1,19 +1,28 @@
 package music2
 
-import music2.management.Composer
-import music2.player.Combiner
-import music2.player.util.Frequencies
+import music2.management.{Composer, JsonParser}
+import music2.player.{Combiner, Riff, SimpleTone}
+import music2.player.util.{Frequencies, Scales}
 import music2.player.util.Notes._
+import music2.player.util.Scales.BluesExt
 
 object Music2 {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
+    val playerOpt = args match {
+      case Array("--path", path) =>
+        JsonParser parse path
+      case Array("--raw", raw) =>
+        JsonParser parseRaw raw
+      case _ =>
+        System.err.println("Usage: music2 [--path | --raw] [<file path> | <JSON string>]")
+        return
+    }
 
-    val st1 = Frequencies get C
-    val st2 = Frequencies get E
-    val st3 = Frequencies get G
-
-    val comb = new Combiner(st1, st2, st3)
-
-    Composer play (comb, 3)
+    playerOpt match {
+      case Some(player) =>
+        Composer play (player, 10)
+      case None =>
+        System.err.println("JSON was malformed, or file not found")
+    }
   }
 }
