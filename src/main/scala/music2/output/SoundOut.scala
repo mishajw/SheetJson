47b2e74f4ae76775529e6244be7df92aeeddce26
Misha Wagner <mishajw@gmail.com>
2016-06-14 14:30:37 +0100

    Make Out and interface, implemented by SoundOut

---
+++ src/main/scala/music2/output/SoundOut.scala
@@ -0,0 +1,116 @@
+package music2.output
+
+import java.util.concurrent.LinkedBlockingQueue
+import javax.sound.sampled.{AudioFormat, AudioSystem, DataLine, SourceDataLine}
+
+import music2.player.PlayableImplicits.Playable
+
+import scala.collection.JavaConverters._
+
+object SoundOut extends Out {
+
+  val sampleRate = 24000
+
+  /**
+    * Input audio is read in these size chunks
+    */
+  private val bufferAmount = 64
+
+  /**
+    * Bytes are read from this queue
+    */
+  private val byteQueue =
+    new LinkedBlockingQueue[Byte]()
+
+  /**
+    * The thread that plays bytes
+    */
+  private val thread = new Thread(new Runnable {
+    def run() = playFromQueue()
+  })
+
+  /**
+    * @return the default audio line
+    */
+  private def defaultLine = {
+    val format =
+      new AudioFormat(sampleRate, 16, 1, true, false)
+
+    val info =
+      new DataLine.Info(classOf[SourceDataLine], format)
+
+    val line =
+      AudioSystem.getLine(info).asInstanceOf[SourceDataLine]
+
+    line.open(format, 4096)
+    line.start()
+
+    line
+  }
+
+  /**
+    * Destroy a line
+    *
+    * @param line
+    */
+  private def destroy(line: SourceDataLine) = {
+    line.drain()
+    line.stop()
+    line.close()
+  }
+
+  /**
+    * Read from the byte queue, and play the results
+    */
+  private def playFromQueue() = {
+    implicit val line = defaultLine
+    var running = true
+
+    while (running) {
+      try {
+        playBytes({
+          for (_ <- 0 until bufferAmount) yield {
+            byteQueue.take()
+          }
+        })
+      } catch {
+        case e: InterruptedException =>
+          running = false
+      }
+    }
+
+    destroy(line)
+  }
+
+  /**
+    * Play bytes to audio
+    *
+    * @param bytes the bytes to play
+    * @param line the line to play them from
+    */
+  private def playBytes(bytes: Traversable[Byte])(implicit line: SourceDataLine) = {
+    line.write(bytes.toArray, 0, bytes.size)
+  }
+
+  /**
+    * Start the playing
+    */
+  def start(): Unit = thread.start()
+
+  /**
+    * Play a playable value
+    */
+  def play[T](x: T)(implicit p: Playable[T]) = {
+    val i = p.to[Int](x)
+    val bytes = Seq(i, i >> 8).map(_.asInstanceOf[Byte])
+    byteQueue.addAll(bytes.asJava)
+  }
+
+  /**
+    * Stop the playing
+    */
+  def stop() = thread.synchronized {
+    thread.interrupt()
+    thread.join()
+  }
+}
