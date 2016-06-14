3ba56bddee3a03bea377cfa777243f7b65f4c236
Misha Wagner <mishajw@gmail.com>
2016-06-14 16:30:55 +0100

    Add JavaDoc

--- src/main/scala/music2/management/Composer.scala
+++ src/main/scala/music2/management/Composer.scala
@@ -5,6 +5,13 @@ import music2.player.PlayableImplicits.Playable
 import music2.player.Player
 
 object Composer {
+
+  /**
+    * Play from a Player
+    * @param player the Player to play from
+    * @param seconds how long to play for
+    * @param out the output
+    */
   def play[T](player: Player[T], seconds: Double, out: Out)(implicit p: Playable[T]): Unit = {
     val steps = (music2.sampleRate * seconds).toInt
 
@@ -15,6 +22,11 @@ object Composer {
     Thread.sleep((seconds * 1000).toInt)
   }
 
+  /**
+    * Play from a player, and create the output
+    * @param player the Player to play from
+    * @param time how long to play for
+    */
   def play[T](player: Player[T], time: Long)(implicit p: Playable[T]): Unit = {
     val out = new SoundAndFileOut("out.pcm")
 
