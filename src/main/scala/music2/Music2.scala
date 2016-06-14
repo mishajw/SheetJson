dcc53848dfbef46929774dcd4b31e8eae45e6fa4
Misha Wagner <mishajw@gmail.com>
2016-06-14 13:51:35 +0100

    Adapt main for new interfaces

--- src/main/scala/music2/Music2.scala
+++ src/main/scala/music2/Music2.scala
@@ -1,11 +1,19 @@
 package music2
 
+import music2.player.SimpleTone
 import music2.util.Output
 
 object Music2 {
   def main(args: Array[String]) {
+    val st = new SimpleTone(261.63)
+
+    val played = for (_ <- 0 until Output.sampleRate * 3)
+      yield st.play
+
+    played.foreach(Output.play(_))
+
     Output start()
-    Output play 0
+    Thread.sleep(5000)
     Output stop()
   }
 }
