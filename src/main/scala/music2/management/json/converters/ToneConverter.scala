20e42f658e5a6689c1d922147708e5b3a8c0565f
Misha Wagner <mishajw@gmail.com>
2016-06-15 19:48:56 +0100

    Allow octaves to be specified in tones

--- src/main/scala/music2/management/json/converters/ToneConverter.scala
+++ src/main/scala/music2/management/json/converters/ToneConverter.scala
@@ -1,13 +1,19 @@
 package music2.management.json.converters
 import music2.player.util.Frequencies.FrequencyOf
 import music2.player.util.Notes
+import music2.player.util.Notes.{AbsoluteNote, Note}
 import music2.player.{Player, Tone}
 import org.json4s.JObject
 import org.json4s.JsonAST.{JDouble, JString}
 
+import scala.util.Try
+
 object ToneConverter extends JsonConverter {
 
   case class JTone(note: Double, waveFunction: String = "sine")
+
+  val rRelativeNote = """([A-Za-z]{1,2})""".r
+  val rAbsoluteNote = """([A-Za-z]{1,2})([\-\d]+)""".r
 
   override val identifier: String = "tone"
 
@@ -16,7 +22,7 @@ object ToneConverter extends JsonConverter {
     // TODO: Handle octaves
     val transformed = json transformField {
       case ("note", JString(n)) =>
-        Notes noteFor n match {
+        noteOf(n) match {
           case Some(note) => ("note", JDouble(note frequency))
           case None => "note" -> JString(n)
         }
@@ -24,6 +30,23 @@ object ToneConverter extends JsonConverter {
 
     transformed.extractOpt[JTone]
       .map(jTone => new Tone(jTone.note, waveFunctionNames(jTone.waveFunction), getSpec(json)))
+  }
+
+  def noteOf(str: String): Option[Note] = {
+    val parts = str match {
+      case rRelativeNote(key) =>
+        (Notes noteFor key, None)
+      case rAbsoluteNote(key, octave) =>
+        (Notes noteFor key, Try(octave.toInt).toOption)
+      case _ =>
+        (None, None)
+    }
+
+    parts match {
+      case (Some(key), Some(octave)) =>
+        Some(AbsoluteNote(key, octave))
+      case (keyOpt, _) => keyOpt
+    }
   }
 
   val waveFunctionNames = Map(
