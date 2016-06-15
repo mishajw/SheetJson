0177058eb44ff3a333235a5340803823a04df551
Misha Wagner <mishajw@gmail.com>
2016-06-15 15:44:55 +0100

    Implement toBytes method

--- src/main/scala/music2/player/Playable.scala
+++ src/main/scala/music2/player/Playable.scala
@@ -1,7 +1,13 @@
 package music2.player
 
+import Playable._
+
 class Playable(private val value: Double) {
-  def toBytes: Seq[Byte] = ???
+  def toBytes: Seq[Byte] = {
+    val i: Int = (value * (rangeInt16 / rangePlayable)).toInt
+
+    Seq(i, i >> 8).map(_.toByte)
+  }
 }
 
 object Playable {
@@ -14,4 +20,11 @@ object Playable {
 
   val default = new Playable(0)
 
+  private val maxInt16: Int = (Math.pow(2, 15) - 1).toInt
+  private val minInt16: Int = (-Math.pow(2, 15)).toInt
+  private val rangeInt16: Int = maxInt16 - minInt16
+
+  private val maxPlayable: Double = 1
+  private val minPlayable: Double = -1
+  private val rangePlayable: Double = maxPlayable - minPlayable
 }
