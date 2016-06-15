60b7493392d0507a65d0a21a242c5c17ad9e17d8
Misha Wagner <mishajw@gmail.com>
2016-06-15 10:40:03 +0100

    Change Riff to have explicitly defined time spans for Players

--- src/main/scala/music2/player/Riff.scala
+++ src/main/scala/music2/player/Riff.scala
@@ -1,30 +1,67 @@
 package music2.player
 import music2.LifeTime
 import music2.player.PlayableImplicits.Playable
+import music2.player.Riff.{PlayerDescription, PlayerDuration, PlayerSpan}
 
 /**
-  * Plays a sequence of notes for certain durations
-  * @param notes the notes and their durations
+  * Plays a sequence of notes for certain time spans
+ *
+  * @param _notes the notes and their spanning times
   */
-class Riff[T](notes: Seq[(Player[T], Double)], _lifeTime: LifeTime = None) extends Player[T](_lifeTime) {
+class Riff[T](_notes: Seq[PlayerDescription[T]], _lifeTime: LifeTime = None) extends Player[T](_lifeTime) {
+
+  /**
+    * Cast all notes to `PlayerSpan`
+    */
+  lazy val notes: Seq[PlayerSpan[T]] = {
+    var cumulativeTime: Double = 0
+
+    for (n <- _notes) yield n match {
+      case ps: PlayerSpan[T] =>
+        cumulativeTime = ps.end ; ps
+      case PlayerDuration(p, d) =>
+        val ps = PlayerSpan(p, cumulativeTime, cumulativeTime + d)
+        cumulativeTime += d
+        ps
+    }
+  }
 
   /**
     * The total duration of the riffs
     */
-  private lazy val riffDuration = notes.map(_._2).sum
+  private lazy val riffDuration = notes.map(_.end).max
 
   override protected def _play(implicit p: Playable[T]): T = {
-    var progress = step % riffDuration
+    val progress = step % riffDuration
 
-    val player = notes.takeWhile({ case (_, d) =>
-      if (progress < 0) {
-        false
-      } else {
-        progress -= d
-        true
-      }
-    }).last._1
+    val playingNotes = notes
+      .filter(n => n.start <= progress && progress <= n.end)
+      .map(_.player)
 
-    player.play
+    new Combiner(playingNotes).play
   }
+}
+
+/**
+  * Singleton holding types used in `Riff`
+  */
+object Riff {
+
+  /**
+    * Description of when a `Player` should play within a `Riff``
+    */
+  sealed trait PlayerDescription[T]
+
+  /**
+    * @param player the `Player` to play
+    * @param duration how long to play
+    */
+  case class PlayerDuration[T](player: Player[T], duration: Double) extends PlayerDescription[T]
+
+  /**
+    * @param player the `Player` to play
+    * @param start when to start playing
+    * @param end when to finish playing
+    */
+  case class PlayerSpan[T](player: Player[T], start: Double, end: Double) extends PlayerDescription[T]
 }
