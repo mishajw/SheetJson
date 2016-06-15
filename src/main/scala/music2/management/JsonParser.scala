fd6bc79c600c8f9bdbff7f180b95bb6334dea896
Misha Wagner <mishajw@gmail.com>
2016-06-15 11:32:02 +0100

    Add JSON string parsing to JsonParser

--- src/main/scala/music2/management/JsonParser.scala
+++ src/main/scala/music2/management/JsonParser.scala
@@ -3,8 +3,11 @@ package music2.management
 import java.io.FileNotFoundException
 
 import music2.player.Player
+import org.json4s.JsonAST.{JObject, JValue}
 
 import scala.io.Source
+import org.json4s._
+import org.json4s.jackson.JsonMethods
 
 /**
   * Parses JSON objects into Player objects
@@ -30,8 +33,22 @@ object JsonParser {
 
   /**
     * Parse a JSON string
-    * @param json the JSON string
+    * @param jsonStr the JSON string
     * @return an option of a player from the JSON string
     */
-  def parseRaw(json: String): Option[Player[Int]] = ???
+  def parseRaw(jsonStr: String): Option[Player[Int]] = {
+    JsonMethods.parseOpt(jsonStr) match {
+      case Some(json) => parseJson(json)
+      case None =>
+        System.err.println("Provided JSON is not valid")
+        None
+    }
+  }
+
+  /**
+    * Parse a JSON object
+    * @param json the JSON object
+    * @return an option of a player from the JSON object
+    */
+  def parseJson(json: JValue): Option[Player[Int]] = ???
 }
