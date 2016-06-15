9a441d7504664fc617f6c0a285e8751ad2cd76bd
Misha Wagner <mishajw@gmail.com>
2016-06-15 20:12:47 +0100

    Rename RandomizerPlayer -> Randomizer

--- src/main/scala/music2/player/filter/Randomizer.scala
+++ src/main/scala/music2/player/filter/Randomizer.scala
@@ -2,7 +2,7 @@ package music2.player.filter
 
 import music2.player.{Playable, Player, PlayerSpec}
 
-class RandomizerPlayer(_child: Player, _spec: PlayerSpec = PlayerSpec()) extends FilterPlayer(_child, _spec) {
+class Randomizer(_child: Player, _spec: PlayerSpec = PlayerSpec()) extends FilterPlayer(_child, _spec) {
   override protected def _play: Playable = {
     child.play * (1 + Math.random() * 0.3)
   }
