07c2b0c22546e06ef64b9f5ebf3f606a91b90af6
Misha Wagner <mishajw@gmail.com>
2016-06-15 20:14:19 +0100

    Add randomness parameter to Randomizer

--- src/main/scala/music2/player/filter/Randomizer.scala
+++ src/main/scala/music2/player/filter/Randomizer.scala
@@ -2,8 +2,11 @@ package music2.player.filter
 
 import music2.player.{Playable, Player, PlayerSpec}
 
-class Randomizer(_child: Player, _spec: PlayerSpec = PlayerSpec()) extends FilterPlayer(_child, _spec) {
+class Randomizer( _child: Player,
+                  val randomness: Double = 1.2,
+                  _spec: PlayerSpec = PlayerSpec()) extends FilterPlayer(_child, _spec) {
+
   override protected def _play: Playable = {
-    child.play * (1 + Math.random() * 0.3)
+    child.play * (1 + Math.random() * randomness)
   }
 }
