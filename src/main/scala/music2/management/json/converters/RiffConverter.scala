d8abd328d7fd9de86cf60fc13ed18efcaa53270b
Misha Wagner <mishajw@gmail.com>
2016-06-15 19:51:56 +0100

    Allow components to be faulty

--- src/main/scala/music2/management/json/converters/RiffConverter.scala
+++ src/main/scala/music2/management/json/converters/RiffConverter.scala
@@ -2,7 +2,7 @@ package music2.management.json.converters
 import music2.management.json.JsonParser
 import music2.player.Player
 import music2.player.composite.Riff
-import music2.player.composite.Riff.{PlayerDuration, PlayerSpan}
+import music2.player.composite.Riff.{PlayerDescription, PlayerDuration, PlayerSpan}
 import org.json4s.JObject
 import org.json4s.JsonAST.{JArray, JDouble}
 
@@ -10,17 +10,18 @@ object RiffConverter extends JsonConverter {
   override val identifier: String = "riff"
 
   override def apply(json: JObject): Option[Player] = {
-    val descriptions = for {
+    val descriptions: Seq[Option[PlayerDescription]] = for {
       JObject(obj) <- json
       ("components", JArray(components)) <- obj
       JArray((jsonComponent: JObject) :: description) <- components
     } yield (JsonParser parseJson jsonComponent, description) match {
       case (Some(player), List(JDouble(start), JDouble(end))) =>
-        PlayerSpan(player, start, end)
+        Some(PlayerSpan(player, start, end))
       case (Some(player), List(JDouble(duration))) =>
-        PlayerDuration(player, duration)
+        Some(PlayerDuration(player, duration))
+      case _ => None
     }
 
-    Some(new Riff(descriptions, getSpec(json)))
+    Some(new Riff(descriptions.flatten, getSpec(json)))
   }
 }
