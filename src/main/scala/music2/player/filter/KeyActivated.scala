464d70f84f58646e534e0c5555a48c8f849a9e8a
Misha Wagner <mishajw@gmail.com>
2016-06-17 18:37:47 +0100

    Change pressed boolean to be atomiuc

--- src/main/scala/music2/player/filter/KeyActivated.scala
+++ src/main/scala/music2/player/filter/KeyActivated.scala
@@ -1,5 +1,7 @@
 package music2.player.filter
 
+import java.util.concurrent.atomic.AtomicBoolean
+
 import com.typesafe.scalalogging.Logger
 import music2.management.KeyListener.KeyCode
 import music2.player.{ListenerPlayer, Playable, Player, PlayerSpec}
@@ -11,24 +13,24 @@ class KeyActivated( _key: KeyCode,
 
   private val log = Logger(getClass)
 
-  private var pressed = false
+  private val pressed = new AtomicBoolean(false)
 
   override val keys: Seq[KeyCode] = Seq(_key)
 
   override protected def _play: Playable = {
     val played = child.play
 
-    if (pressed) played
-    else         Playable.default
+    if (pressed.get()) played
+    else               Playable.default
   }
 
   override def keyPressed(kc: KeyCode): Unit = {
     log.debug(s"Key pressed: $kc")
-    pressed = true
+    pressed set false
   }
 
   override def keyReleased(kc: KeyCode): Unit = {
     log.debug(s"Key released: $kc")
-    pressed = false
+    pressed set true
   }
 }
