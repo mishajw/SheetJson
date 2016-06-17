96a4c19ad796af237d2116fc9cbee9cacea9f436
Misha Wagner <mishajw@gmail.com>
2016-06-17 17:02:37 +0100

    Add CompositeConverter type to reduce boilerplate for composite players

--- src/main/scala/music2/management/json/converters/CompositeConverter.scala
+++ src/main/scala/music2/management/json/converters/CompositeConverter.scala
@@ -1,8 +1,22 @@
 package music2.management.json.converters
 
-/**
-  * Created by misha on 17/06/16.
-  */
-trait CompositeConverter {
+import music2.player.Player
+import music2.player.composite.CompositePlayer
+import org.json4s.{JObject, JValue}
+import org.json4s.JsonAST.JArray
 
+trait CompositeConverter[T] extends JsonConverter {
+  override def apply(json: JObject): Option[Player] = {
+    val cs: Seq[T] = (for {
+      JObject(obj) <- json
+      ("components", JArray(components)) <- obj
+      component <- components
+    } yield convertWrapped(component)).flatten
+
+    applyWithComponents(cs, json)
+  }
+
+  protected def convertWrapped(json: JValue): Option[T]
+
+  protected def applyWithComponents(cs: Seq[T], json: JObject): Option[Player]
 }
--- src/main/scala/music2/management/json/converters/RiffConverter.scala
+++ src/main/scala/music2/management/json/converters/RiffConverter.scala
@@ -1,27 +1,26 @@
 package music2.management.json.converters
 import music2.management.json.JsonParser
 import music2.player.Player
-import music2.player.composite.Riff
 import music2.player.composite.Riff.{PlayerDescription, PlayerDuration, PlayerSpan}
-import org.json4s.JObject
+import music2.player.composite.{CompositePlayer, Riff}
 import org.json4s.JsonAST.{JArray, JDouble}
+import org.json4s.{JObject, JValue}
 
-object RiffConverter extends JsonConverter {
+object RiffConverter extends CompositeConverter[PlayerDescription] {
   override val identifier: String = "riff"
 
-  override def apply(json: JObject): Option[Player] = {
-    val descriptions: Seq[Option[PlayerDescription]] = for {
-      JObject(obj) <- json
-      ("components", JArray(components)) <- obj
-      JArray((jsonComponent: JObject) :: description) <- components
-    } yield (JsonParser parseJson jsonComponent, description) match {
-      case (Some(player), List(JDouble(start), JDouble(end))) =>
-        Some(PlayerSpan(player, start, end))
-      case (Some(player), List(JDouble(duration))) =>
-        Some(PlayerDuration(player, duration))
-      case _ => None
-    }
-
-    Some(new Riff(descriptions.flatten, getSpec(json)))
+  override protected def convertWrapped(json: JValue): Option[PlayerDescription] = json match {
+    case JArray((jsonComponent: JObject) :: description) =>
+      (JsonParser parseJson jsonComponent, description) match {
+        case (Some(player), List(JDouble(start), JDouble(end))) =>
+          Some(PlayerSpan(player, start, end))
+        case (Some(player), List(JDouble(duration))) =>
+          Some(PlayerDuration(player, duration))
+        case _ => None
+      }
+    case _ => None
   }
+
+  override protected def applyWithComponents(cs: Seq[PlayerDescription], json: JObject): Option[Player] =
+    Some(new Riff(cs, getSpec(json)))
 }
