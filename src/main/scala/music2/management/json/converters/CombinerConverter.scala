1913e416c0b2d2409f8c7e12b3f9bf6b2aa052cd
Misha Wagner <mishajw@gmail.com>
2016-06-17 17:10:56 +0100

    Make combiner converter fit new model for composite converters

--- src/main/scala/music2/management/json/converters/CombinerConverter.scala
+++ src/main/scala/music2/management/json/converters/CombinerConverter.scala
@@ -1,14 +1,17 @@
 package music2.management.json.converters
+import music2.management.json.JsonParser
 import music2.player.Player
 import music2.player.composite.Combiner
-import org.json4s.JObject
+import org.json4s.{JObject, JValue}
 
-object CombinerConverter extends JsonConverter {
+object CombinerConverter extends CompositeConverter[Player] {
   override val identifier: String = "combiner"
 
-  override def apply(json: JObject): Option[Player] = {
-    Some(new Combiner(
-      getComponents(json),
-      getSpec(json)))
+  override protected def convertWrapped(json: JValue): Option[Player] = json match {
+    case j: JObject => JsonParser parseJson j
+    case _ => None
   }
+
+  override protected def applyWithComponents(cs: Seq[Player], json: JObject): Option[Player] =
+    Some(new Combiner( cs, getSpec(json)))
 }
