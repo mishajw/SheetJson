3ca950dea090a83737b22759dd1e94853ba1ee7d
Misha Wagner <mishajw@gmail.com>
2016-06-17 15:09:19 +0100

    Add field for a list of keys to listen to

--- src/main/scala/music2/player/ListenerPlayer.scala
+++ src/main/scala/music2/player/ListenerPlayer.scala
@@ -3,6 +3,10 @@ package music2.player
 import music2.management.KeyListener.KeyCode
 
 trait ListenerPlayer {
+
+  val keys: Seq[KeyCode]
+
   def keyPressed(kc: KeyCode): Unit = {}
+
   def keyReleased(kc: KeyCode): Unit = {}
 }
