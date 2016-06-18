5c7da2e81b97f3555bca25c177410299f2d8c06f
Misha Wagner <mishajw@gmail.com>
2016-06-18 12:18:00 +0100

    Add constructors for Time case classes to be converted between eachotehr

--- src/main/scala/music2/util/Time.scala
+++ src/main/scala/music2/util/Time.scala
@@ -17,10 +17,24 @@ object Time {
   val bpm: BPM = 100
 
   /**
+    * How many beats are there per bar
+    * If 4, the time signature is 4/4
+    * If 3, the time signature is 3/4
+    */
+  val beatsPerBar: Int = 4
+
+  /**
     * Represents seconds
     * Used for calculating wave functions
     */
   case class Seconds(value: Double)
+
+  object Seconds {
+    def apply(a: Absolute): Seconds =
+      Seconds(a.value / sampleRate)
+    def apply(b: Bars): Seconds =
+      Seconds(((bpm / beatsPerBar) / 60) * b.value)
+  }
 
   /**
     * Represents amount of ticks called
@@ -28,9 +42,23 @@ object Time {
     */
   case class Absolute(value: Int)
 
+  object Absolute {
+    def apply(s: Seconds): Absolute =
+      Absolute((s.value * sampleRate).toInt)
+    def apply(b: Bars): Absolute =
+      Absolute(Seconds(b))
+  }
+
   /**
     * Represents musical bars, set by a BPM
     * Used for timing how long things last, such as durations, spans, and looping lengths
     */
   case class Bars(value: Double)
+
+  object Bars {
+    def apply(s: Seconds): Bars =
+      Bars(s.value / (bpm / beatsPerBar / 60))
+    def apply(a: Absolute): Bars =
+      Bars(Seconds(a))
+  }
 }
