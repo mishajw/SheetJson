f228be4a6289a7c13cf829733c5e81bf10a63a45
Misha Wagner <mishajw@gmail.com>
2016-07-09 18:19:27 +0200

    Add JavaDoc and implementation comments to RawFile

--- src/main/scala/music2/player/origin/RawFile.scala
+++ src/main/scala/music2/player/origin/RawFile.scala
@@ -12,12 +12,17 @@ import scala.util.control.Breaks._
 
 class RawFile(path: String, _spec: PlayerSpec) extends Player(_spec) {
 
+  /**
+    * Playables taken from the input file
+    */
   private val playablesFromFile: Vector[Playable] = {
     val rawInput = AudioSystem.getAudioInputStream(new File(path))
+
+    // Change the input so that it's the same format as the rest of the system
     val formattedInput: AudioInputStream = AudioSystem.getAudioInputStream(SoundOut.format, rawInput)
 
+    // Read in the bytes
     val bytes: ArrayBuffer[Byte] = new ArrayBuffer()
-
     breakable { while (true) {
       val buffer = Array.fill(32){0.toByte}
 
@@ -27,6 +32,7 @@ class RawFile(path: String, _spec: PlayerSpec) extends Player(_spec) {
       }
     }}
 
+    // Format the bytes to be playables
     bytes
       .grouped(2)
       .map { case bs =>
@@ -37,6 +43,9 @@ class RawFile(path: String, _spec: PlayerSpec) extends Player(_spec) {
       .toVector
   }
 
+  /**
+    * The size of the Playables list. Expensive to compute with large files, so is stored here.
+    */
   private lazy val playablesSize = playablesFromFile.size
 
   override protected def _play: Playable = {
