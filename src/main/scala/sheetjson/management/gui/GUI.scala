ebb86c6efa832962f2553a58a7fb52ba65239095
Misha Wagner <mishajw@gmail.com>
2016-12-06 17:57:46 +0000

    Change default windows size

--- src/main/scala/sheetjson/management/gui/GUI.scala
+++ src/main/scala/sheetjson/management/gui/GUI.scala
@@ -6,7 +6,7 @@ import javax.swing.JFrame
 object GUI {
   private val frame = {
     val f = new JFrame("sheetjson")
-    f.setSize(10, 10)
+    f.setSize(800, 600)
     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
     f.setContentPane(new View())
     f.setVisible(true)
