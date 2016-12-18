256e3d541f2ec9820ab1fc141524fcc95e087c0e
Misha Wagner <mishajw@gmail.com>
2016-12-18 12:41:22 +0000

    Move ActivatablePlayer into correct directory

---
+++ src/main/scala/sheetjson/player/ActivatablePlayer.scala
@@ -0,0 +1,24 @@
+package sheetjson.player
+
+import sheetjson.management.KeyListener._
+
+trait ActivatablePlayer extends ListenerPlayer {
+
+  protected var _isActive = false
+
+  def isActive = _isActive
+
+  override def keyPressed(kc: KeyCode): Unit = {
+    _isActive = true
+    activate()
+  }
+
+  override def keyReleased(kc: KeyCode): Unit = {
+    _isActive = false
+    deactivate()
+  }
+
+  def activate()
+
+  def deactivate()
+}
