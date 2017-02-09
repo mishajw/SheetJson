63df7036d7ed7aabcd8caace18ccac42974b7123
Misha Wagner <mishajw@gmail.com>
2017-02-09 08:21:42 +0000

    Change defaultVolume for origin players to 0.2

--- src/main/scala/sheetjson/player/origin/OriginPlayer.scala
+++ src/main/scala/sheetjson/player/origin/OriginPlayer.scala
@@ -5,10 +5,10 @@ import sheetjson.util.Identifiable
 
 abstract class OriginPlayer(_spec: PlayerSpec) extends Player(_spec) {
   /**
-    * Set to 0.5, because while filter/composite players need to not quieten children, the children shouldn't all be
+    * Set to less than 1, because while filter/composite players need to not quieten children, the children shouldn't all be
     * 100% volume otherwise distortion will occur
     */
-  override val defaultVolume: Double = 0.5
+  override val defaultVolume: Double = 0.2
 
   override def identifiableChildren: Seq[Identifiable] = super.identifiableChildren
 }
